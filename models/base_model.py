a31aa39727d0f11d4a3cb60cd918d6f2f9b668a6
PreciousIfeaka <preciousifeaka@gmail.com>
2022-10-26 21:47:25 -0700

    updated models

--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,19 @@ class BaseModel:
     """This class defines all common attributes/methods for other classes
     """
 
-    def __init__(self):
-        """initializes all instances"""
+    def __init__(self, *args, **kwargs):
+        """initializes instances from the dict representation"""
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                """iterates through the attributes of instance in the dict"""
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    """the datetime is converted from str to object format"""
+                if key == "__class__":
+                    continue
+                """The class key is not added"""
+                setattr(self, key, value)
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
