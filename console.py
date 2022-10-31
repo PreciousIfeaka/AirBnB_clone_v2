0a6d9cd7ac2ec11b4b7461a1a93baede4b962218
PreciousIfeaka <preciousifeaka@gmail.com>
2022-10-31 13:30:12 -0700

    finished all console tasks

--- console.py
+++ console.py
@@ -3,9 +3,11 @@
 """This module contains a mini shell console
 """
 
+import re
+import ast
 import models
 import cmd
-from shlex import split
+import shlex
 from models.base_model import BaseModel
 from models.user import User
 from models.city import City
@@ -63,6 +65,9 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
         elif len(args) > 1:
             key = args[0] + "." + args[1]
+            for obj in models.storage.all().keys():
+                if obj == key:
+                    print(models.storage.all()[key])
 
     def do_create(self, arg):
         """Creates a new instance of BaseModel, saves it
@@ -78,28 +83,9 @@ class HBNBCommand(cmd.Cmd):
             print(new_inst.id)
             new_inst.save()
 
-    def do_show(self, arg):
-        """Prints the string representation of an instance based on
-        the class name and id"""
-        if len(arg) == 0:
-            print("** class name missing **")
-            return
-        argv = arg.split()
-        if argv[0] not in classes:
-            print("** class doesn't exist **")
-            return
-        elif len(argv) < 2:
-            print("** instance id missing **")
-            return
-        elif len(argv) > 1:
-            key = argv[0] + "." + argv[1]
-            if key in models.storage.all():
-                print(models.storage.all()[key])
-            else:
-                print("** no instance found **")
-                return
-
     def do_all(self, arg):
+        """Prints all string representation of all instances based or not
+        on the class name"""
         if not arg:
             lists = []
             try:
@@ -122,7 +108,9 @@ class HBNBCommand(cmd.Cmd):
                 print(lists)
 
     def do_update(self, arg):
-        args = arg.split()
+        """Updates an instance based on the class name and id by adding
+        or updating attribute (save the change into the JSON file)"""
+        args = shlex.split(arg)
         key = args[0] + "." + args[1]
         if len(args) == 0:
             print("** class name is missing **")
@@ -187,78 +175,13 @@ class HBNBCommand(cmd.Cmd):
             finally:
                 return
 
-    def do_all(self, arg):
-        '''Prints all string representation of all instances based or
-        not on the class name'''
-        if not arg:
-            list_inst = []
-            for key, val in models.storage.all().items():
-                list_int.append(str(val))
-            if not list_inst:
-                print("storage is empty!")
-                return
-            else:
-                print(list_inst)
-                return
-        argv = arg.split()
-        if argv[0] not in classes:
-            print("** class doesn't exist **")
-            return
-        else:
-            list_inst = []
-            for key, val in models.storage.all().items():
-                if str(key.split('.')[0]) == argv[0]:
-                    list_inst.append(str(val))
-            if not list_inst:
-                return
-            else:
-                print(list_inst)
-                return
-
-    def do_update(self, arg):
-        """Updates an instance based on the class name and id by adding
-        or updating attribute (save the change into the JSON file)"""
-        if not arg:
-            print("** class name missing **")
-            return
-        argv = shlex.split(arg)
-        if argv[0] not in classes:
-            print("** class doesn't exist **")
-            return
-        if len(argv) == 1:
-            print("** instance id missing **")
-            return
-        else:
-            try:
-                key = argv[0] + "." + argv[1]
-                if key in models.storage.all():
-                    models.storage.all()[key]
-            except Exception:
-                print("** no instance found **")
-                return
-        if len(argv) == 2:
-            print("** attribute name missing **")
-            return
-        elif len(argv) == 3:
-            print("** value missing **")
-            return
-        else:
-            key = argv[0] + "." + argv[1]
-            try:
-                try:
-                    value = int(argv[3])
-                except ValueError:
-                    value = float(argv[3])
-            except ValueError:
-                value = argv[3].strip(":\"'")
-            attr = argv[2].strip(":\"'")
-            setattr(models.storage.all()[key], attr, value)
-            models.storage.save()
-
     def default(self, line):
         """
         This takes care of all self defined functions
         """
+        inst_ids = re.search(r'\d[a-z0-9-]+', line)
+        if inst_ids:
+            inst_id = inst_ids.group(0)
         cmd_list = line.split(".")
         if len(cmd_list) == 2:
             class_name = cmd_list[0]
@@ -268,8 +191,22 @@ class HBNBCommand(cmd.Cmd):
                 elif cmd_list[1] == "all()":
                     self.do_all(class_name)
                 elif cmd_list[1][:4] == "show":
-                    inst_id = cmd_list[1][6:42]
                     self.do_show("{} {}".format(class_name, inst_id))
+                elif cmd_list[1][:7] == "destroy":
+                    self.do_destroy("{} {}".format(class_name, inst_id))
+                elif cmd_list[1][:6] == "update":
+                    cmd_attrs = line.split(",")
+                    if cmd_attrs[1][1] == "{":
+                        dict_vals = ast.literal_eval(
+                                re.search(r'({.+})', line).group(0))
+                        for key, value in dict_vals.items():
+                            self.do_update("{} {} {} {}".format(
+                                class_name, inst_id, key, value))
+                    else:
+                        cmd_attr = cmd_attrs[1].strip()
+                        cmd_attr1 = cmd_attrs[2].split(")")[0]
+                        self.do_update("{} {} {} {}".format(
+                            class_name, inst_id, cmd_attr, cmd_attr1))
 
 
 if __name__ == '__main__':
