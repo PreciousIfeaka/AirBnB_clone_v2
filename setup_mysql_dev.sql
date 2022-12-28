db3dd7606592e91a081a190f0fe5f6d875f08903
PreciousIfeaka <preciousifeaka@gmail.com>
2022-12-28 06:55:40 -0800

    created databases and users for database storage

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,10 @@
+-- This script prepares a MySql server for this project.
+-- Creates a database 'hbnb_dev_db'
+-- Creates a new user 'hbnb_dev' in localhost
+-- Sets the password of hbnb_dev to 'hbnb_dev_pwd'.
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
