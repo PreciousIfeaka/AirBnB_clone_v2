55c4b4b5db104158f817660fdeae287a75a78686
PreciousIfeaka <preciousifeaka@gmail>
2023-01-18 20:30:26 +0000

    updated directory

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,7 +22,7 @@ def c_text(text):
 
 @app.route('/python/<path:text>', strict_slashes=False)
 @app.route('/python/', strict_slashes=False)
-def python_text(text = 'is cool'):
+def python_text(text='is cool'):
     return "Python " + text.replace('_', ' ')
 
 
