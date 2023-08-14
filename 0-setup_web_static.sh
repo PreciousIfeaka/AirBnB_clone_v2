0937a3c5908c799167e22fef5d6f411f7412c14b
PreciousIfeaka <preciousifeaka@gmaill.com>
2023-08-14 01:33:00 +0000

    configured nginx

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# This script sets up my web servers for deployment of web_static folder.
+#...The conditions to be satisfied can be deciphered from the code.
+
+prev_path="/data/web_static/releases/test/"
+sym_link="/data/web_static/current"
+
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+echo "Testing my process" > /data/web_static/releases/test/index.html
+
+if [ -L "$sym_link" ];
+then
+	rm -rf "$sym_link"
+fi
+# Above condition deletes the symbolic link if it exists
+
+sudo ln -s "$prev_path" "$sym_link"
+sudo chown -R ubuntu:ubuntu /data/
+sudo service nginx start
