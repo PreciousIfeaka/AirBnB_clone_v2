c7fc9d817cadab42b486650f07e74c4d427ffec4
PreciousIfeaka <preciousifeaka@gmaill.com>
2023-08-14 02:19:06 +0000

    configured nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # This script sets up my web servers for deployment of web_static folder.
-#...The conditions to be satisfied can be deciphered from the code.
+# ...The conditions to be satisfied can be deciphered from the code.
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
@@ -12,3 +12,4 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
+exit 0
