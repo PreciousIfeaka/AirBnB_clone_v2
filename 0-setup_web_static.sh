57cadec1ab8160e17b346dcfec7a936cde112529
PreciousIfeaka <preciousifeaka@gmaill.com>
2023-08-14 01:39:37 +0000

    configured nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,10 +13,11 @@ echo "Testing my process" > /data/web_static/releases/test/index.html
 
 if [ -L "$sym_link" ];
 then
-	rm -rf "$sym_link"
+        rm -rf "$sym_link"
 fi
 # Above condition deletes the symbolic link if it exists
 
 sudo ln -s "$prev_path" "$sym_link"
 sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '58i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
