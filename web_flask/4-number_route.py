a87cc947e3cf952228fe557db09bba76ea3dd466
PreciousIfeaka <preciousifeaka@gmaill.com>
2023-08-21 18:55:46 +0000

    added an integer route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This script starts a flask web app using 3 different routes
+to print different text
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)  # creates an instance of Flask class
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    """runs the functions"""
+    app.run(host='0.0.0.0', port=5000)
