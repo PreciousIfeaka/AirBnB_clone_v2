2db946438087d95c92eda0f90f3d090ae7b9f1e8
PreciousIfeaka <preciousifeaka@gmaill.com>
2023-08-21 10:56:45 +0000

    routes to two different urls

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -17,3 +17,8 @@ def hello_hbnb():
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
+
+
+if __name__ == "__main__":
+    """runs the functions"""
+    app.run(host='0.0.0.0', port=5000)
